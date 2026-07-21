d7618fea8fc13d12c2e7bbae04e4673885f7d697
Emilio Kariuki <emilio113kariuki@gmail.com>
2026-07-22 01:09:23 +0300

    chore(formula): update ngstone to v0.2.0

--- Formula/ngstone.rb
+++ Formula/ngstone.rb
@@ -2,27 +2,27 @@ class Ngstone < Formula
   desc "Self-hosted HTTP tunnel, a personal ngrok alternative"
   homepage "https://github.com/emilio-kariuki/ngstone"
   license "MIT"
-  version "0.1.0"
+  version "0.2.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/emilio-kariuki/ngstone/releases/download/v0.1.0/ngstone_darwin_arm64.tar.gz"
-      sha256 "806aa9f36188f1436f95ed2e9cf6678246e3e113e77ec2826f9f3778e88549b4"
+      url "https://github.com/emilio-kariuki/ngstone/releases/download/v0.2.0/ngstone_darwin_arm64.tar.gz"
+      sha256 "bc0c0286aeccf2d00ffabf980baf9d5f4d6d26d621f898d4f46decb0ce263f3d"
     end
     on_intel do
-      url "https://github.com/emilio-kariuki/ngstone/releases/download/v0.1.0/ngstone_darwin_amd64.tar.gz"
-      sha256 "4de5cad5977f27956996ee1335adeeb2e6b49e1bbc712b333a387215160bf37b"
+      url "https://github.com/emilio-kariuki/ngstone/releases/download/v0.2.0/ngstone_darwin_amd64.tar.gz"
+      sha256 "246fe839e8cc643964c5aa1aeb8fcd75024c39910933be9bcb4c3f9b603d5527"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/emilio-kariuki/ngstone/releases/download/v0.1.0/ngstone_linux_arm64.tar.gz"
-      sha256 "d3066eb83b29f837e212f6d111a3fad9dccd879f0a80d3ffbf207459aa60288b"
+      url "https://github.com/emilio-kariuki/ngstone/releases/download/v0.2.0/ngstone_linux_arm64.tar.gz"
+      sha256 "74c001e5579f05e282612e27c4d6514b2d6aa8273cbb77276496a0895abb5ace"
     end
     on_intel do
-      url "https://github.com/emilio-kariuki/ngstone/releases/download/v0.1.0/ngstone_linux_amd64.tar.gz"
-      sha256 "859a7863e61cb6eed4deaa1888b999071e92434daad6132579524af59c5ed496"
+      url "https://github.com/emilio-kariuki/ngstone/releases/download/v0.2.0/ngstone_linux_amd64.tar.gz"
+      sha256 "bf19469bad0c58851983b9dcebe799d144b517177f0ad3bb6c7940c619d38c43"
     end
   end
 
